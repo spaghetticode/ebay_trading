b912f0f52dabefed42bf4032154ab2374feb8e0e
andrea longhi <andrea@spaghetticode.it>
2011-12-28 17:39:29 +0100

    use ENV variables if ebay_trading.yml is not present

--- lib/ebay_trading/config.rb
+++ lib/ebay_trading/config.rb
@@ -1,6 +1,16 @@
 module EbayTrading
   module Config
     class << self
+      # this config class variable is overwritten if there is a ebay_trading
+      # yml file in config folder (rails root)
+      @@config = {
+        'url'        => ENV['URL'],
+        'app_name'   => ENV['APP_NAME'],
+        'dev_name'   => ENV['DEV_NAME'],
+        'auth_token' => ENV['AUTH_TOKEN'],
+        'cert_name'  => ENV['CERT_NAME']
+      }
+
       %w[dev_name app_name cert_name auth_token url].each do |method|
         define_method method do
           @@config[method]
--- lib/ebay_trading/railtie.rb
+++ lib/ebay_trading/railtie.rb
@@ -1,8 +1,10 @@
 module EbayTrading
   class Railtie < ::Rails::Railtie
+    # if there is a config file present, use it instead of the env variables
     initializer 'ebay_trading.load_api_configuration' do |app|
       EbayTrading::Config.module_eval do
-        class_variable_set '@@config', YAML.load_file("#{Rails.root}/config/ebay_trading.yml")[Rails.env]
+        config_file = Rails.root.join('config/ebay_trading.yml')
+        class_variable_set('@@config', YAML.load_file(config_file)[Rails.env]) if File.file?(config_file)
       end
     end
   end
